124b42f5ae53a05aeae5596a74b71023774ae004
Adam <adamant415@gmail.com>
2011-04-04 00:30:05 -0400

    build script, initial initramfs config, clean script, defconig

---
+++ Kernel/build_kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WORK=`pwd`
+cd ..
+
+if [ ! -d arm-2009q3 ]; then
+	tarball="arm-2009q3-67-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2"
+	if [ ! -f "$tarball" ]; then
+		wget http://www.codesourcery.com/public/gnu_toolchain/arm-none-linux-gnueabi/"$tarball"
+	fi
+	tar -xjf "$tarball"
+fi
+
+cd $WORK
+rm -f kernel_update-"$tag".zip
+make clean mrproper
+make ARCH=arm nemesis2all_defconfig
+make -j `expr $(grep processor /proc/cpuinfo | wc -l) + 1` CROSS_COMPILE=../arm-2009q3/bin/arm-none-linux-gnueabi- \
+	ARCH=arm HOSTCFLAGS="-g -O3"
+
+cp -p arch/arm/boot/zImage update/kernel_update
+cd update
+zip -r -q kernel_update.zip . 
+mv kernel_update.zip ../kernel_update-"$tag".zip
